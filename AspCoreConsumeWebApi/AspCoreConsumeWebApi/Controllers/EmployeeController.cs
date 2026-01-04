c605a66e4548f93a94d7ff96ac889b2d593675be
Keshav Sharma <sharmakeshav802@gmail.com>
2026-01-04 15:46:07 +0530

    consume api
    consume api

---
+++ AspCoreConsumeWebApi/AspCoreConsumeWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,139 @@
+﻿using AspCoreConsumeWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AspCoreConsumeWebApi.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        //private string baseUrl = "https://localhost:7253/api/";
+        //private HttpClient client=new HttpClient();
+        private HttpClient _client;
+        public EmployeeController(IHttpClientFactory httpClientFactory)
+        {
+            _client = httpClientFactory.CreateClient("ApiClient");
+        }
+        public async Task<IActionResult> Index()
+        {
+            var responce = await _client.GetAsync("employee/");
+            if (responce.IsSuccessStatusCode)
+            {
+                string data = await responce.Content.ReadAsStringAsync();
+                if (data != null) {
+                    var employee = JsonConvert.DeserializeObject<List<Employee>>(data);
+                    if (employee != null)
+                    {
+                        return View(employee);
+                    }
+                    ViewBag.ErrorMessage = "Employee is not found";
+
+                }
+
+            }
+            return BadRequest("Data not found");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if (employee == null) {
+                ViewBag.ErrorMessage = "Invalid data";
+                return View(employee);
+            }
+            var content = new StringContent(JsonConvert.SerializeObject(employee), Encoding.UTF8, "application/json");
+            var responce = await _client.PostAsync("employee", content);
+            if (responce.IsSuccessStatusCode)
+            {
+                TempData["SuccessMessage"] = "Employee added successfully";
+                return RedirectToAction("Index");
+            }
+            ViewBag.ErrorMessage = "failed to create employee ";
+            return View(employee);
+
+        }
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var responce = await _client.GetAsync($"employee/{Id}");
+            if (responce.IsSuccessStatusCode)
+            {
+                string data = await responce.Content.ReadAsStringAsync();
+                if (data != null)
+                {
+                    var employee = JsonConvert.DeserializeObject<Employee>(data);
+                    if (employee != null)
+                    {
+                        return View(employee);
+                    }
+                    ViewBag.ErrorMessage = "Employee is not found";
+
+                }
+
+            }
+            return BadRequest("Data not found");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Employee employee)
+        {
+            if (employee == null)
+            {
+                ViewBag.ErrorMessage = "Invalid data";
+                return View(employee);
+            }
+            var content = new StringContent(JsonConvert.SerializeObject(employee), Encoding.UTF8, "application/json");
+            var responce = await _client.PutAsync($"employee/{employee.empId}", content);
+            if (responce.IsSuccessStatusCode)
+            {
+                TempData["SuccessMessage"] = "Employee updated successfully";
+                return RedirectToAction("Index");
+            }
+            ViewBag.ErrorMessage = "failed to create employee ";
+            return View(employee);
+
+        }
+
+        public async Task<IActionResult> Details(int Id)
+        {
+            var responce = await _client.GetAsync($"employee/{Id}");
+            if (responce.IsSuccessStatusCode)
+            {
+                string data = await responce.Content.ReadAsStringAsync();
+                if (data != null)
+                {
+                    var employee = JsonConvert.DeserializeObject<Employee>(data);
+                    if (employee != null)
+                    {
+                        return View(employee);
+                    }
+                    ViewBag.ErrorMessage = "Employee is not found";
+
+                }
+
+            }
+            return BadRequest("Data not found");
+        }
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var response = await _client.DeleteAsync($"employee/{Id}");
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["SuccessMessage"] = "Employee Deleted";
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Failed to delete employee";
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ AspCoreConsumeWebApi/AspCoreConsumeWebApi/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace AspCoreConsumeWebApi.Models
+{
+
+    public class Employee
+    {
+        public int empId { get; set; }
+        public string empName { get; set; }
+        public int empAge { get; set; }
+        public string empGender { get; set; }
+        public DateTime empDOJ { get; set; }
+        public string designation { get; set; }
+        public int salary { get; set; }
+    }
+
+}
